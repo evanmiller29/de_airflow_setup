7e995abd8983c1c0a124429303da4d65b9653eab
evanmiller29 <evanmiller29@gmail.com>
2020-08-24 13:48:43 +1200

    Adding file sensors to pipeline

--- forex_data_pipeline.py
+++ forex_data_pipeline.py
@@ -1,5 +1,7 @@
 from airflow import DAG
 from datetime import datetime, timedelta
+from airflow.sensors.http_sensor import HttpSensor
+from airflow.contrib.sensors.file_sensor import FileSensor
 
 default_args = {
     "owner": "airflow",
@@ -11,5 +13,29 @@ default_args = {
     "retries": 1,
     "retry_delay": timedelta(minutes=5)
 
-
 }
+
+with DAG(
+    dag_id="forex_data_pipeline",
+    schedule_interval="@daily",
+    default_args=default_args,
+    catchup=False) as dag:
+
+    is_forex_rates_available = HttpSensor(
+        task_id="is_forex_rates_available",
+        method="GET",
+        http_conn_id="forex_api",
+        endpoint="latest",
+        response_check=lambda response: "rates" in response.text,
+        poke_interval=5,
+        timeout=20
+
+    )
+
+    is_forex_currencies_file_available = FileSensor(
+        task_id="is_forex_currencies_file_available",
+        fs_conn_id="forex_path",
+        filepath="forex_currencies.csv",
+        poke_interval=5,
+        timeout=20
+    )
